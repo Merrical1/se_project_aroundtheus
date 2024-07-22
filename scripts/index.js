c91740c7ae1436f356ec644d0c27d7dd62a14083
Merrical Miller <merricalmiller@gmail.com>
2024-07-21 19:55:16 -0500

    complete

--- scripts/index.js
+++ scripts/index.js
@@ -24,13 +24,56 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   }
 ];
-
+/* Elements */
 const profileEditButton = document.querySelector("#profile-edit-button");
-const profileEditModal = document.querySelector("#profile-edit-moda;");
+const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileEditModalClose = document.querySelector("#profile-edit-modal-close");
+const profileTitle = document.querySelector('.profile__title');
 
+const profileDescription = document.querySelector('.profile__description');
+const profileTitleInput = document.querySelector('#profile-title-input')
+const profileDescriptionInput = document.querySelector('#profile-description-input')
+
+const profileEditForm = profileEditModal.querySelector('.modal__form');
+
+/* Function */
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData){
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector('.card__image');
+  const cardTitleEl = cardElement = cardElement.querySelector('.card__title');
+  cardTitleEl.textContent = cardData.name;
+  return cardElement;
+}
+
+
+/* Events Handlers*/
+function handleProfileEditSubmit (e) => {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+
+}
+
+/* Event Listeners */
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
-  profileEditModalClose.classList.add("modal_close");
 });
+
+profileEditCloseButton.addEventListener("click", closePopup);
+
+
+profileEditForm.addEventListener("submit" , handleProfileEditSubmit);
+
+ initialCards.forEach(cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.append(cardElement);
+ };
+
 
